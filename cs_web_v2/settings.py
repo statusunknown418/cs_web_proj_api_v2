3d3eeb8ec697c7bdef31611857c05fce09d5f00d
alvaro <alvarodevcode@outlook.com>
2022-07-11 10:14:00 -0500

    feat: added cors middleware

--- cs_web_v2/settings.py
+++ cs_web_v2/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +52,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# This is not a really good practice for production but it's ok for now
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'cs_web_v2.urls'
 
